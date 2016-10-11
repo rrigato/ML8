aaedb91adfbbdcb1232fb336dc8056b3ce1eb531
rrigato <rrigato@horizon.csueastbay.edu>
2016-10-11 18:59:13 -0500

    Got a list of the number of distinct categoricals for each variable

--- EDA.py
+++ EDA.py
@@ -18,7 +18,23 @@ class severityModel:
 		print(self.train.shape)
 		print(self.test.head())
 		print(self.train.shape)
-			
+		
+	def getUnique(self):
+		'''
+			Gets a list of distinct values
+		'''
+		self.categoryUnique = [None] * 115
+		i = 0
+		'''
+			Iterating over each categorical variable to 
+			see how many unique values are in each
+			Stores the result in self.categoryUnique
+		'''
+		for cols in self.train.ix[:,2:117]:
+			self.categoryUnique[i] = len(np.unique(self.train.loc[:,cols]))
+			i += 1
 if __name__ == '__main__':
 	insuranceObj = severityModel()
+	insuranceObj.getUnique()
+	print(insuranceObj.categoryUnique)
 	
