ec0e03377a5c62d33c017d2293bf264ebc960ba3
rrigato <rrigato@horizon.csueastbay.edu>
2016-10-31 18:48:16 -0500

    1000 tree tdboost cross validation

--- baseModel.R
+++ baseModel.R
@@ -12,25 +12,38 @@ loadTest = function()
 }
 
 
-toFactor = function(train, test)
+toFactor = function(train)
 {
 
 	for (i in 2:117)
 	{
 		train[,i] = as.factor(train[,i])
-		test[,i] = as.factor(test[,i])
+		#test[,i] = as.factor(test[,i])
 	}
-	
+	return(train)
 }
 
 getTrain =  function(train)
 {
 	set.seed(1234)
-	ranTrain = sample(1:nrow(train), floor(nrow(train) * .8) )
+	ranTrain = sample(1:nrow(train), floor(nrow(train) * .6) )
 	return(ranTrain)
 }
 
 
+makeTrain = function(ranTrain, train)
+{
+	Train = train[ranTrain,]
+	return(Train)
+}
+
+makeHoldout = function(ranTrain, train)
+{
+	#gets the observations that are not in the train dataset for the 
+	#test dataset
+	holdout = train[which(!((1:nrow(train)) %in% ranTrain) ),]
+	return(holdout)
+}
 ##############################################################
 #Main entry point for the script
 #
@@ -42,9 +55,20 @@ library(TDboost)
 
 train = loadTrain()
 test = loadTest()
+train = toFactor(train)
+test = toFactor(test)
+
+Train = makeTrain(getTrain(train), train)
+holdout = makeHoldout(getTrain(train), train)
+#everything except the id
+tdFit = TDboost(loss ~ .,  n.trees = 1000, cv.folds = 5, data = Train[,-c(1)])
 
 
-TDboost(loss ~ ., data = train)
+#predict the observations
+output = predict.TDboost(tdFit, holdout, 50)
+MAE <- sum(abs(temp[,3]- temp[,1])) / length(temp)
+
+
 
 
 
