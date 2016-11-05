0ba564be6537dd92e10ec2bddfc8b6827c42efa0
rrigato <rrigato@horizon.csueastbay.edu>
2016-11-05 10:17:36 -0500

    Attempting to one-hot encode all of the categorical variables

--- baseModel.R
+++ baseModel.R
@@ -11,6 +11,21 @@ loadTest = function()
 	test = as.data.frame(fread("C:\\Users\\Punkiehome1\\Downloads\\allstateKaggle\\test.csv", sep = ','))
 }
 
+encodeCategorical(train)
+{
+	#one hot encodes all categorical variables,
+	#suppresses the intercept term
+	encodedCat =  model.matrix(~.-1, train[,2:116] )
+
+	#combines one hot encoded variables with continuous features and 
+	#outcome variable
+	allVars = as.data.frame(cbind(train[,1], encodedCat, train[,117:ncol(train)]))
+
+	#levels of 
+	notInTrain = names(encoded2)[which(!(names(encoded2)  %in% names(encodedCat)) )]
+	return(allVars)
+}
+
 
 toFactor = function(train)
 {
@@ -55,19 +70,40 @@ library(TDboost)
 
 train = loadTrain()
 test = loadTest()
+
+train = encodeCategorical(train)
+test = encodeCategorical(test)
+
 train = toFactor(train)
 test = toFactor(test)
 
 Train = makeTrain(getTrain(train), train)
 holdout = makeHoldout(getTrain(train), train)
 #everything except the id
-tdFit = TDboost(loss ~ .,  n.trees = 1000, cv.folds = 5, data = Train[,-c(1)])
+tdFit = TDboost(loss ~ .,  n.trees = 10, cv.folds = 5, data = Train[,-c(1)])
 
 
+
+crossResult = TDboost.perf(tdFit, method = "cv")
 #predict the observations
-output = predict.TDboost(tdFit, holdout, 50)
+output = predict.TDboost(tdFit, holdout, 1000)
+temp = cbind(output, holdout$id, holdout$loss)
 MAE <- sum(abs(temp[,3]- temp[,1])) / length(temp)
 
+
+test[which(as.character(test$cat89) == "F"), 'cat89'] = 'NA'
+
+#fixing variables that had no levels in train dataset
+test[,'cat92'] = as.character(test[,'cat92'])
+test[which(as.character(test$cat92) == "E"), 'cat92'] = 'A'
+test[which(as.character(test$cat92) == "G"), 'cat92'] = 'A'
+test[,'cat96'] = as.character(test[,'cat96'])
+test[which(as.character(test$cat96) == "H"), 'cat96'] = 'A'
+
+test[c(67038, 88842), 'cat89'] = 'A'
+output1 = predict.TDboost(tdFit, test, 1000)
+
+Result1 
 
 
 
