2d43f62e785a2010d75f48de7f10f6de442ab7d5
rrigato <rrigato@horizon.csueastbay.edu>
2016-11-13 13:49:34 -0600

    Attempting to define the base learning models paramters

--- stacking.R
+++ stacking.R
@@ -80,6 +80,45 @@ test = test[1:nrow(test), colnames(test)[2:ncol(test)]]
 
 learner <- c("h2o.glm.wrapper", "h2o.randomForest.wrapper", 
              "h2o.gbm.wrapper", "h2o.deeplearning.wrapper")
+
+
+#adding custom learning functions
+h2o.glm.1 <- function(..., alpha = 0.0) h2o.glm.wrapper(..., alpha = alpha)
+h2o.glm.2 <- function(..., alpha = 0.5) h2o.glm.wrapper(..., alpha = alpha)
+h2o.glm.3 <- function(..., alpha = 1.0) h2o.glm.wrapper(..., alpha = alpha)
+h2o.randomForest.1 <- function(..., ntrees = 200, nbins = 50, seed = 1) h2o.randomForest.wrapper(..., ntrees = ntrees, nbins = nbins, seed = seed)
+h2o.randomForest.2 <- function(..., ntrees = 200, sample_rate = 0.75, seed = 1) h2o.randomForest.wrapper(..., ntrees = ntrees, sample_rate = sample_rate, seed = seed)
+h2o.randomForest.3 <- function(..., ntrees = 200, sample_rate = 0.85, seed = 1) h2o.randomForest.wrapper(..., ntrees = ntrees, sample_rate = sample_rate, seed = seed)
+h2o.randomForest.4 <- function(..., ntrees = 200, nbins = 50, balance_classes = TRUE, seed = 1) h2o.randomForest.wrapper(..., ntrees = ntrees, nbins = nbins, balance_classes = balance_classes, seed = seed)
+h2o.gbm.1 <- function(..., ntrees = 100, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, seed = seed)
+h2o.gbm.2 <- function(..., ntrees = 100, nbins = 50, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, nbins = nbins, seed = seed)
+h2o.gbm.3 <- function(..., ntrees = 100, max_depth = 10, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, max_depth = max_depth, seed = seed)
+h2o.gbm.4 <- function(..., ntrees = 100, col_sample_rate = 0.8, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, col_sample_rate = col_sample_rate, seed = seed)
+h2o.gbm.5 <- function(..., ntrees = 100, col_sample_rate = 0.7, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, col_sample_rate = col_sample_rate, seed = seed)
+h2o.gbm.6 <- function(..., ntrees = 100, col_sample_rate = 0.6, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, col_sample_rate = col_sample_rate, seed = seed)
+h2o.gbm.7 <- function(..., ntrees = 100, balance_classes = TRUE, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, balance_classes = balance_classes, seed = seed)
+h2o.gbm.8 <- function(..., ntrees = 100, max_depth = 3, seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, max_depth = max_depth, seed = seed)
+h2o.gbm.9 <- function(..., ntrees = 1000, max_depth = 35,
+learn_rate = .01,  seed = 1) h2o.gbm.wrapper(..., ntrees = ntrees, max_depth = max_depth,
+learn_rate = learn_rate, seed = seed)
+h2o.deeplearning.1 <- function(..., hidden = c(500,500), activation = "Rectifier", epochs = 50, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, seed = seed)
+h2o.deeplearning.2 <- function(..., hidden = c(200,200,200), activation = "Tanh", epochs = 50, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, seed = seed)
+h2o.deeplearning.3 <- function(..., hidden = c(500,500), activation = "RectifierWithDropout", epochs = 50, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, seed = seed)
+h2o.deeplearning.4 <- function(..., hidden = c(500,500), activation = "Rectifier", epochs = 50, balance_classes = TRUE, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, balance_classes = balance_classes, seed = seed)
+h2o.deeplearning.5 <- function(..., hidden = c(100,100,100), activation = "Rectifier", epochs = 50, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, seed = seed)
+h2o.deeplearning.6 <- function(..., hidden = c(50,50), activation = "Rectifier", epochs = 50, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, seed = seed)
+h2o.deeplearning.7 <- function(..., hidden = c(100,100), activation = "Rectifier", epochs = 50, seed = 1)  h2o.deeplearning.wrapper(..., hidden = hidden, activation = activation, seed = seed)
+
+
+
+
+
+
+learner <- c("h2o.glm.wrapper",
+             "h2o.randomForest.1", "h2o.randomForest.2",
+             "h2o.gbm.1", "h2o.gbm.6", "h2o.gbm.8", "h2o.gbm.9",
+             "h2o.deeplearning.1", "h2o.deeplearning.6", "h2o.deeplearning.7")
+
 metalearner <- "h2o.deeplearning.wrapper"
 
 family = "gaussian"
@@ -95,7 +134,7 @@ system.time(fit <- h2o.ensemble(x = x,
                                 family = family,
                                 learner = learner,
                                 metalearner = metalearner,
-                                cvControl = list(V = 5, shuffle = TRUE)))
+                                cvControl = list(V = 3, shuffle = TRUE)))
 
 result = predict(fit, newdata = test)
 
@@ -103,10 +142,10 @@ result2 = matrix(nrow=nrow(test), ncol = 2)
 result2 = as.data.frame(result2)
 names(result2) = c("ID", "loss")
 
-result2$ID = as.vector(test$ID)
+result2$ID = as.vector(testID)
 result2$loss = as.vector(result$pred)
 
-write.csv(result2, "C:\\Users\\Punkiehome1\\Downloads\\allstateKaggle\\holdoutPred.csv", row.names = FALSE)
+write.csv(result2, "C:\\Users\\Punkiehome1\\Downloads\\allstateKaggle\\result2.csv", row.names = FALSE)
 
 
 
